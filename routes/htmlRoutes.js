7df753ac8f3832f9bd3fd8aa4231bf7b69846af9
jmc <13514717+jmcoleman@users.noreply.github.com>
2022-01-31 23:57:56 -0500

    removed jquery and added styling

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
-const filePath = './public/'
-const resolvedPath = path.resolve(filePath)
+const filePath = '../public/'
+const resolvedPath = path.resolve(__dirname, filePath)
 
 router.route('/').get(function (req, res) {
 	res.sendFile(path.join(resolvedPath, 'home.html'))
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
-app.use(express.static(__dirname + '/public'))
 
-app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// serve static folders
+app.use(express.static(__dirname + '/public'))
 
 // mount routes
 const htmlRoutes = require('./routes/htmlRoutes')
